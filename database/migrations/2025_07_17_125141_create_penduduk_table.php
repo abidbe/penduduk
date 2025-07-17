1b619073e628f8064856f5d7dd372ed95b6019f0
abidbe <abid.i.ft20@mail.umy.ac.id>
2025-07-17 19:59:55 +0700

    remove jumlah_penduduk column from kabupaten table and add penduduk migration

--- database/migrations/2025_07_17_125141_create_penduduk_table.php
+++ database/migrations/2025_07_17_125141_create_penduduk_table.php
@@ -11,10 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('kabupaten', function (Blueprint $table) {
+        Schema::create('penduduk', function (Blueprint $table) {
             $table->id();
-            $table->string('nama_kabupaten');
-            $table->foreignId('provinsi_id')->constrained('provinsi')->onDelete('cascade');
+            $table->string('nik')->unique();
+            $table->string('nama');
+            $table->foreignId('kabupaten_id')->constrained('kabupaten')->onDelete('cascade');
+            $table->integer('umur');
+            $table->text('alamat');
             $table->timestamps();
         });
     }
@@ -24,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('kabupaten');
+        Schema::dropIfExists('penduduk');
     }
 };
